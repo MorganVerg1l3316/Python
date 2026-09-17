2a24a51c3bf8bd1a5a955220735ba04c7b7fb274
Morgan <mwoodward2022@gmail.com>
2026-09-17 11:43:48 +0100

    Delete Codeacademy/LearnPython3 directory

--- Codeacademy/LearnPython3/Basics/Block_Letters.py
+++
@@ -1,8 +0,0 @@
-# Block letters project
-print('M       M   W       W')
-print('M M   M M   W       W')
-print('M M   M M   W       W')
-print('M   M   M   W   W   W')
-print('M       M   W W   W W')
-print('M       M   W W   W W')
-print('M       M   W       W')
